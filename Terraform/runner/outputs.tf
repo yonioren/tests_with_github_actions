f03fe65a402b7d0b2cab0cb817ae072d417effff
yonioren <thescrew3692@gmail.com>
2025-12-01 22:02:05 +0200

    Update outputs.tf

--- Terraform/runner/outputs.tf
+++ Terraform/runner/outputs.tf
@@ -2,10 +2,6 @@ output "private_key_path" {
   value = var.private_key
 }
 
-output "instance_id" {
-  value = module.runner_instance.instance_id
-}
-
 output "runner_public_ip" {
   value = module.runner_instance.public_ip
 }
