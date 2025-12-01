2089fbb1798ccab050248b3559e760d8afea3a35
yonioren <thescrew3692@gmail.com>
2025-12-01 22:17:45 +0200

    Update outputs.tf

--- Terraform/k8s/outputs.tf
+++ Terraform/k8s/outputs.tf
@@ -1,21 +1,9 @@
-output "master_instance_id" {
-  value = module.master.instance_id
-}
-
 output "master_public_ip" {
   value = module.master.public_ip
 }
 
 output "master_private_ip" {
   value = module.master.private_ip
-}
-
-output "workers_instance_ids" {
-  value = module.workers[*].instance_id
-}
-
-output "workers_private_ips" {
-  value = module.workers[*].private_ip
 }
 
 output "alb_dns_name" {
